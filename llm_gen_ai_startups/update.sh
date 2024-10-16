c9d204041bb256b4fcb3fc7e48465ea598fba5cb
Feng Peng <pengfeng@gmail.com>
2024-10-16 14:01:44 -0700

    Update scripts

--- llm_gen_ai_startups/update.sh
+++ llm_gen_ai_startups/update.sh
@@ -2,14 +2,21 @@
 
 set -e -u
 
+CUR_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 ts=$(date +%Y%m%d)
 
+set +u
 if [ "" == "$eds_home" ]; then
     eds_home=$(realpath ~/llmeds)
 fi
+set -u
 
-git co main
-git pull
+pushd . >/dev/null 2>&1 
+cd "${CUR_PATH}"
+
+# git co main
+# git pull
 git co -b data_"$ts"
 git push --set-upstream origin data_"$ts"
 cp "${eds_home}"/src/applications/startup_list/startup_list.latest.json startup_list.json
@@ -18,3 +25,5 @@ python plot.py
 git add .
 git commit -m "update data $ts"
 git push
+
+popd >/dev/null 2>&1 
